0ac28c41d99f1ec58a9f90c6651ac1a2f4045605
AaronLi <aaronli@xm.remarkablefoods.com>
2025-05-31 21:56:17 +0800

    build

---
+++ main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import App from './App';
+// 弹出框
+import DialogBox from './components/DialogBox/DialogBox';
+
+// main.js
+
+
+Vue.prototype.$basePath = process.env.VUE_APP_BASE_PATH || "";
+
+Vue.config.productionTip = false
+
+// 全局组件
+Vue.component('DialogBox', DialogBox);
+//mescroll
+import MescrollBody from "@/components/mescroll-uni/mescroll-body.vue"
+import MescrollUni from "@/components/mescroll-uni/mescroll-uni.vue"
+Vue.component('mescroll-body', MescrollBody)
+Vue.component('mescroll-uni', MescrollUni)
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
